db5f85257e39036c1a9e3ff8c41a84ca8c99323c
john1eng <john1eng@hotmail.com>
2020-05-08 22:24:11 -0400

    update package.json, tsconfig, buildspec.yml

---
+++ build/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+    return res.send('Hello World!');
+});
+app.listen(port, function () { return console.log("listening at http://localhost:" + port); });

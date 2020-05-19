d60b46f5b19098ae821423200b82f86dc850a632
john1eng <john1eng@hotmail.com>
2020-05-19 11:01:29 -0400

    remove cores origin to original state *

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -8,7 +8,7 @@ import { Request, Response } from "express";
 
 export function corsFilter(req: Request, resp: Response, next) {
 
-    resp.header('Access-Control-Allow-Origin', '* , http://localhost:3000, http://project01-ui.s3-website-us-east-1.amazonaws.com'); 
+    resp.header('Access-Control-Allow-Origin', '*'); 
     resp.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     resp.header('Access-Control-Allow-Credentials', 'true');
     resp.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');

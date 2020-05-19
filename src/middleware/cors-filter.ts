c1bd31d0c08d46386a5a91ee20151c5b22048dce
john1eng <john1eng@hotmail.com>
2020-05-19 11:03:09 -0400

    nothing

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -8,7 +8,7 @@ import { Request, Response } from "express";
 
 export function corsFilter(req: Request, resp: Response, next) {
 
-    resp.header('Access-Control-Allow-Origin', '*'); 
+    resp.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
     resp.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     resp.header('Access-Control-Allow-Credentials', 'true');
     resp.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
